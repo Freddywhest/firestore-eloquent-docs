fe84f99c6d8650ec39add78e517b8a6581a57cd7
Freddywhest <5181010477@st.uew.edu.gh>
2024-03-27 01:19:48 +0000

    Version 2.x documentation 📜 :rocket:

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,55 +4,60 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Firestore Eloquent',
-  tagline: 'Eloquent for firestore.',
-  favicon: 'img/logo-s.png',
+  title: "Firestore Eloquent",
+  tagline: "Eloquent for firestore.",
+  favicon: "img/logo-s.png",
   plugins: [
-    [require.resolve("@easyops-cn/docusaurus-search-local"),
-      { hashed: true }
-    ],
+    [require.resolve("@easyops-cn/docusaurus-search-local"), { hashed: true }],
   ],
 
   // Set the production url of your site here
-  url: 'https://firestore-eloquent.netlify.app',
+  url: "https://firestore-eloquent.netlify.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'FreddyWhest', // Usually your GitHub org/user name.
-  projectName: 'firestore-eloquent', // Usually your repo name.
+  organizationName: "FreddyWhest", // Usually your GitHub org/user name.
+  projectName: "firestore-eloquent", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/FreddyWhest/firestore-eloquent-docs/tree/main/',
+            "https://github.com/FreddyWhest/firestore-eloquent-docs/tree/main/",
+          lastVersion: "current",
+          versions: {
+            current: {
+              label: "2.x",
+            },
+          },
         },
+
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -62,43 +67,45 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo_card.png',
+      image: "img/logo_card.png",
       /* announcementBar: {
-        id: 'support_us',
-        content:
-          'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
-        backgroundColor: '#e8d7ff',
-        textColor: '#091E42',
-        isCloseable: false,
+        id: "announcementBar-3", // Increment on change
+        // content: `⭐️ If you like Docusaurus, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/facebook/docusaurus">GitHub</a> and follow us on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/docusaurus">Twitter ${TwitterSvg}</a>`,
+        content: `🎉️ <b><a target="_blank" href="https://docusaurus.io/blog/releases/3.0">Docusaurus v3.0</a> is now out!</b> 🥳️`,
       }, */
       navbar: {
-        title: 'Firestore Eloquent',
+        title: "Firestore Eloquent",
         logo: {
-          alt: 'Firestore Eloquent Logo',
-          src: 'img/logo-s.png',
+          alt: "Firestore Eloquent Logo",
+          src: "img/logo-s.png",
         },
         items: [
           {
-            href: '/docs',
-            position: 'left',
-            label: 'Docs',
+            href: "/docs",
+            position: "left",
+            label: "Docs",
           },
           {
-            href: 'https://github.com/FreddyWhest/firestore-eloquent',
-            label: 'GitHub',
-            position: 'right',
+            type: "docsVersionDropdown",
+            position: "right",
+            //dropdownActiveClassDisabled: true,
+          },
+          {
+            href: "https://github.com/FreddyWhest/firestore-eloquent",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} Firestore Eloquent. Built by <a href="https://github.com/FreddyWhest">Alfred Nti.</a>`,
       },
       prism: {
         theme: prismThemes.dracula,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['php', 'bash'],
-      }
+        additionalLanguages: ["php", "bash"],
+      },
     }),
 };
 
